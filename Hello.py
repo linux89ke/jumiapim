57f2991f1956ead70318251220dd225e4ac531ac
linux89ke <161024953+linux89ke@users.noreply.github.com>
2024-03-20 13:32:25 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,6 +3,18 @@ import streamlit as st
 # Set page title and favicon
 st.set_page_config(page_title="Welcome to My Jumia App", page_icon=":wave:")
 
+# Custom CSS to change the background color
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: orange;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Header
 st.title("Welcome to My Jumia App!")
 st.markdown("This is my work app")
@@ -13,5 +25,4 @@ st.write("""
 This app is designed to [briefly describe the purpose of your app]. It provides [list some key features or functionalities]. 
 
 Feel free to explore and interact with the different components!
-
-
+""")
