5db55ee8cb31c3ad981612c132e2bb4f62cb25d5
linux89ke <161024953+linux89ke@users.noreply.github.com>
2024-03-21 12:26:36 +0300

    Update Files.py

--- pages/Files.py
+++ pages/Files.py
@@ -2,16 +2,19 @@ import streamlit as st
 import pandas as pd
 
 def merge_csv_files(output_file, uploaded_files):
-    important_columns = ["SellerName", "Name", "Brand", "PrimaryCategory"]
-    dataframes = []
-    
+    dfs = []
     for file in uploaded_files:
-        df = pd.read_csv(file)
-        df = df[important_columns]  # Select only the important columns
-        dataframes.append(df)
-    
-    merged_df = pd.concat(dataframes, ignore_index=True)
-    return merged_df
+        try:
+            df = pd.read_csv(file)[["SellerName", "Name", "Brand", "PrimaryCategory"]]
+            dfs.append(df)
+        except pd.errors.ParserError:
+            st.warning(f"Ignored invalid file: {file.name}")
+
+    if dfs:
+        merged_df = pd.concat(dfs, ignore_index=True)
+        return merged_df
+    else:
+        return None
 
 def add_seller_id(merged_df):
     sellers_df = pd.read_excel("sellers.xlsx")
@@ -29,12 +32,15 @@ def main():
         
         if st.button("Merge CSV files"):
             merged_df = merge_csv_files(output_file, uploaded_files)
-            merged_df_with_seller_id = add_seller_id(merged_df)
-            st.dataframe(merged_df_with_seller_id)
-            
-            # Download button for the merged DataFrame
-            csv = merged_df_with_seller_id.to_csv(index=False)
-            st.download_button(label="Download CSV", data=csv, file_name=output_file, mime='text/csv')
+            if merged_df is not None:
+                merged_df_with_seller_id = add_seller_id(merged_df)
+                st.dataframe(merged_df_with_seller_id)
+                
+                # Download button for the merged DataFrame
+                csv = merged_df_with_seller_id.to_csv(index=False)
+                st.download_button(label="Download CSV", data=csv, file_name=output_file, mime='text/csv')
+            else:
+                st.warning("No valid files to merge.")
 
 if __name__ == "__main__":
     main()
